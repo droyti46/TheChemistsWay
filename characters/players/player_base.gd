6cc062f61fd1656d52af90ba70e9d9569b7b6bad
Никита Бакутов <nikitabakutov2008@gmail.com>
2024-09-19 09:25:00 +0700

    Добавлена папка img

--- characters/players/player_base.gd
+++ characters/players/player_base.gd
@@ -3,16 +3,17 @@ extends CharacterBody2D
 const SPEED = 400.0
 
 func _physics_process(_delta):
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Получение вектора направления
 	var direction = Input.get_vector("player_left",
 									 "player_right",
 									 "player_up",
 									 "player_down")
 	
+	# Тело двигается в нужном направлении
 	velocity = direction * SPEED
 	move_and_slide()
 	
+	# Обработка
 	if Input.is_action_just_pressed("player_left"):
 		%AnimatedSprite.flip_h = true
 	elif Input.is_action_just_pressed("player_right"):
